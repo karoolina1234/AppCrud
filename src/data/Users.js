4df1bd77504f1ead93d450c3b789101b6332c678
karoolina1234 <karoolina.sm@gmail.com>
2021-05-22 00:12:27 -0300

    criaçao de dados

---
+++ src/data/Users.js
@@ -0,0 +1,33 @@
+export default[
+    {
+        id: 1,
+        name: "João silva",
+        email: "joao.silva@email.com",
+        avatarUrl:'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png'
+    },
+    {
+        id: 2,
+        name: "Mariana Aguiar",
+        email: "mariana@email.com",
+        avatarUrl:'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+    },
+    {
+        id: 3,
+        name: "Matheus santos",
+        email: "matheus@email.com",
+        avatarUrl:'https://cdn.pixabay.com/photo/2016/03/31/20/11/avatar-1295575_960_720.png'
+    },
+    {
+        id: 4,
+        name: "Juliana martins",
+        email: "juh@email.com",
+        avatarUrl:'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027369_960_720.png'
+    },
+    {
+        id: 5,
+        name: "Betania",
+        email: "betania@email.com",
+        avatarUrl:'https://cdn.pixabay.com/photo/2016/03/29/05/46/cat-1287594_960_720.png'
+    },
+
+]
